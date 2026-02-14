a0cf51fc56e7b09787aacdc67f2060ac7b6edcde
Denis Maximenko <manlok71@gmail.com>
2026-02-14 15:56:30 +0400

    Platforming character WIP

--- Player3D/platforming_character_3d.gd
+++ Player3D/platforming_character_3d.gd
@@ -1,12 +1,13 @@
-class_name FloatingPlayerCharacter3D
+class_name PlatformingCharacter3D
 extends PlayerCharacter3D
+
 
 func _ready() -> void:
 	super._ready()
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
-	#_process_gravity(delta)
+	_process_gravity(delta)
 	_process_input(delta, speed, acceleration, deceleration)
 	_process_velocity()
 
@@ -22,20 +23,31 @@ func _unhandled_input(event: InputEvent) -> void:
 
 @export_category("Movement")
 @export var speed : float = 10.0
+@export var jump_velocity : float = 10.0
 @export var acceleration : float = 20.0
 @export var deceleration : float = 100.0
 
 func _process_input(delta : float, desired_speed : float, acceleration_rate : float, deceleration_rate : float) -> void:
-	#if not is_processing_input():
-		#return
-	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
 	# Get the input direction and handle the movement/deceleration.
-	var input_dir := JamUtils.get_move_input_dir_3d()
-	var direction := (transform.basis * input_dir).normalized()
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := JamUtils.get_move_input_dir_2d()
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var horizontal_direction := Vector2(direction.x, direction.z)
+	var horizontal_velocity := Vector2(velocity.x, velocity.z) 
 	if direction:
-		velocity = velocity.move_toward(direction * desired_speed, acceleration_rate * delta)
+		horizontal_velocity = horizontal_velocity.move_toward(horizontal_direction * desired_speed, acceleration_rate * delta)
 	else:
-		velocity = velocity.move_toward(Vector3.ZERO, deceleration_rate * delta)
+		horizontal_velocity = horizontal_velocity.move_toward(Vector2.ZERO, deceleration_rate * delta)
+	velocity.x = horizontal_velocity.x
+	velocity.z = horizontal_velocity.y
 
 #endregion
 
